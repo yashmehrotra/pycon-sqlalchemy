b023e11415db03662863e928e38e9b358cc381b7
Yash Mehrotra <yashmehrotra95@gmail.com>
2015-05-31 15:23:48 +0530

    Delete to be added, also make it modular

---
+++ main.py
@@ -0,0 +1,49 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+ 
+engine = create_engine('sqlite:///:memory:') # Add echo=True for viewing SQL Queries
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+ 
+ 
+class User(Base):
+    __tablename__ = 'users'
+ 
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+ 
+    def __repr__(self):
+        return "<User(name='%s', email='%s')>" % (self.name, self.email)
+ 
+ 
+# import pdb;pdb.set_trace()
+Base.metadata.create_all(engine)
+ 
+user = [ User(name='Yash', email='yashmehrotra95@gmail.com'),
+         User(name='Avijit', email='526avijit@gmail.com'),
+         User(name='Bruce', email='bruce@wayne.com') ]
+
+session = Session()
+# add_all is used for adding a list
+session.add_all(user)
+session.commit()
+
+query = session.query(User)
+print 'All users so far:'
+for row in query:
+    print row.name, row.email
+
+query = session.query(User).filter(User.email == 'yashmehrotra95@gmail.com')[0]
+query.name = 'Yash Mehrotra'
+session.add(query)
+session.commit()
+
+query = session.query(User)
+print '\nAll users after update:'
+for row in query:
+    print row.name, row.email
+
+session.close()
